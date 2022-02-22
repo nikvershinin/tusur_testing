77a08d021b2bb27a2ea12764a2db39a010e0d505
nikvershinin <nik_vershinin@mail.ru>
2022-02-22 23:36:35 +0500

    add new two tests for TestNG (DynamicTest1 and DynamicTestNG)

---
+++ unit-online-tusur/src/test/java/online_tusur/unit_online_tusur/DynamicTest1.java
@@ -0,0 +1,25 @@
+package online_tusur.unit_online_tusur;
+
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+
+public class DynamicTest1 {
+	  //Factory with Dataprovider
+@Factory
+	Object[] factoryMethod1() {
+	return new Object[] {new FirstNameTest1()};}}
+
+class FirstNameTest1 {
+	@DataProvider (name="firstNamesProvider")
+	public Object[][] firstNamesProvider(){
+		return new Object[][] {
+			{"Cristiano", new Student("Cristiano","Ronaldo", 26)},
+			{"Bruno", new Student("Bruno","Fernandes", 26)}};}
+			
+@Test (dataProvider="firstNamesProvider")			
+public void firstNames(String name, Student s) {
+	Assert.assertEquals(name, s.getFirstName());
+}
+		}
---
+++ unit-online-tusur/src/test/java/online_tusur/unit_online_tusur/DynamicTestNg.java
@@ -0,0 +1,35 @@
+package online_tusur.unit_online_tusur;
+
+import org.testng.Assert;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+
+public class DynamicTestNg {
+  //Factory with Parameters
+	@Factory
+	
+	Object[] factoryMethod() {
+		return new Object[] {
+				new TestFactory(26, 26),
+				new TestFactory(28, 28),
+				new TestFactory(30, 30)};
+	}
+	}
+				
+class TestFactory {
+	private Student s = new Student();
+	private int expected, actual;
+	public TestFactory(int expected, int actual) {
+			this.expected = expected;
+			this.actual = actual;
+					}
+					
+
+	@Test
+	void testAge() {
+	s.setAge(actual);
+	int actualValue = s.getAge();
+	Assert.assertEquals(expected, actualValue);
+	}
+	}
